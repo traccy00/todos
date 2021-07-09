cf0d04974d0c81db0869469ec981aad3fcb03d91
Le Thi Tieu Mai <mailtt@timebird.org>
2021-07-09 14:05:00 +0700

    todos done

---
+++ src/views/Todos.vue
@@ -0,0 +1,88 @@
+<template>
+    <div class="container mx-auto">
+        <h1 class="mt-8 text-2xl">Todos</h1>
+
+        <div class="mt-3">
+            <div class="grid grid-cols-12 gap-4">
+                <div class="col-span-6 space-y-4 overflow-y-auto px-1" style="height: 500px">
+                    <div v-for="(todo,index) in todos" :key="index" class="p-8 bg-white shadow-md rounded flex items-center justify-between" :class="{'bg-green-200': todo.done}">
+                        <!-- todo name -->
+                        <div>
+                            <div>{{ todo.text }}</div>
+                            <div class="text-gray-500 text-sm">{{ todo.createdAt.toString() }}</div>
+                        </div>
+                        <!-- button -->
+                        <div class="space-x-2">
+                            <button @click="removeTodo(index)" class="px-2 text-red-600" title="Remove todo">&times;</button>
+                            <button v-if="!todo.done" @click="markAsDone(index)" class="px-2 text-red-600" title="Mark as done">&check;</button>
+                            <button v-else @click="markAsUndone(index)" class="px-2 text-red-600" title="Mark as undone">&#8630;</button>
+                        </div>
+                    </div>
+                    <div v-if="todos.length === 0" class="px-8 py-16 bg-white text-gray-700 shadow-sm rounded text-sm">
+                        You dont have any task to do.
+                    </div>
+                </div>
+                
+                <div class="col-span-6">
+                    <div class="p-8 bg-white shadow-sm rounded">
+                        <h2 class="text-xl">Add a todo</h2>
+                        <input v-model="todoText" @keydown.enter="addTodo" type="text" class="p-2 mt-4 border rounded w-full">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import { defineComponent, reactive, ref } from "@vue/runtime-core"
+
+export default defineComponent({
+    setup() {
+        const todos = reactive([
+            // {
+            //     text: "First todo",
+            //     createdAt: new Date(),
+            //     done: false,
+            // }
+        ]);
+        const todoText = ref(""); //const todoText = {value: ""}
+
+        function addTodo() {
+            todos.unshift({
+                text: todoText.value,
+                createdAt: new Date(),
+                done: false
+            })
+
+            todoText.value = "";
+        }
+
+        function markAsDone(index) {
+            todos[index].done = true;
+            // console.log(this.index);
+        }
+
+        function markAsUndone(index) {
+            todos[index].done = false;
+            // console.log(this.index);
+        }
+
+        function removeTodo(index) {
+            if(!confirm("Are you sure?")) {
+                return
+            }
+            todos.splice(index, 1)
+        }
+        
+        return {
+            todos,
+            todoText,
+            addTodo,
+            markAsDone,
+            markAsUndone,
+            removeTodo
+        }
+    }
+})
+</script>
